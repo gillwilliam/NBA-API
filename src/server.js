23b0c99d2cec18dbb3e8742d869fb98a5cc5a4df
gillwilliam <gill.william@gmail.com>
2017-08-07 23:59:06 -0400

    Adding aggregation endpoints

--- src/server.js
+++ src/server.js
@@ -47,11 +47,47 @@ router.get("/nbateam/:team", (req,res) => {
   })
 })
 
+router.get("/average/:team", (req,res) => {
+  axios.get("http://stats.nba.com/stats/leagueLeaders?LeagueID=00&PerMode=PerGame&Scope=S&Season=2016-17&SeasonType=Regular+Season&StatCategory=PTS")
+  .then( (response) => {
+
+    var filtered = new Array();
+    response.data.resultSet.rowSet.map((elem) => {
+      if(elem[3] == req.params.team){
+        filtered.push(elem);
+      }
+    })
+
+    res.json({"test": statAverage(response.data.resultSet.headers, formatJSONArray(response.data.resultSet.headers, filtered))});
+  })
+})
+
+function statAverage (headers, playersx) {
+
+  var Averages = {};
+  var notNums=["PLAYER_ID","PLAYER", "TEAM"];
+  console.log(playersx[0].MIN);
+  headers.map( (title) => {
+    if(notNums.indexOf(title) == -1){
+    var unrounded = playersx.reduce((sum, value) => {
+      return sum + value[title];
+    }, 0) / playersx.length;
+
+    console.log("test" + unrounded);
+    Averages[title] = Math.round(unrounded*1000)/1000;
+    console.log("test2" + (Math.round(unrounded*1000)/1000));
+  }
+  })
+
+  return Averages;
+
+}
+
 function formatJSONArray(headers, playersx){
 
-  var Players = {};
+  var Players = [];
   playersx.map((player) => {
-    Players[player[2]] = formatJSONPlayer(headers,player);
+    Players.push(formatJSONPlayer(headers,player));
   })
 
   return Players;
