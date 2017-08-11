af80250f3bdeb599e0e5af5676585a0fed0079aa
William Gill <gill.william@gmail.com>
2017-08-11 18:11:41 -0400

    Works with full teams

--- src/routes/PlayerGameLogs.js
+++ src/routes/PlayerGameLogs.js
@@ -3,6 +3,7 @@ const axios = require('axios');
 const Utils = require('./Utils');
 const moment = require('moment');
 const AllPlayers = require('./AllPlayers');
+const Promise = require('bluebird');
 
 
 var URL = "http://stats.nba.com/stats/playergamelogs?DateFrom=&DateTo=&GameSegment=&LastNGames=0&LeagueID=00&Location=&MeasureType=Base&Month=0&OpponentTeamID=0&Outcome=&PORound=0&PaceAdjust=N&PerMode=Totals&Period=0&PlayerID=";
@@ -12,62 +13,118 @@ var SEASONDATES = {
   "2015-16": ["11/01/2015", "04/01/2016"]
 }
 
-
-router.get("/getSeasonStats/:year/:playerName", (req, res) => {
-  var formattedPlayerName = (req.params.playerName).split("-");
-  formattedPlayerName= formattedPlayerName[1] + ", " + formattedPlayerName[0];
+function getSeasonStatsPlayer(year, playerName) {
+  var formattedPlayerName = (playerName).split("-");
+  formattedPlayerName = formattedPlayerName[1] + ", " + formattedPlayerName[0];
 
   console.log(formattedPlayerName);
-  var playerInfo = AllPlayers.filter( (player) => {
+  var playerInfo = AllPlayers.filter((player) => {
     return (player[1].toLowerCase() == formattedPlayerName.toLowerCase());
   })
   console.log(playerInfo);
   //201566
-  var completeURL = URL + playerInfo[0][0] + "&PlusMinus=N&Rank=N&Season=" + req.params.year + "&SeasonSegment=&SeasonType=Regular+Season&ShotClockRange=&VsConference=&VsDivision="
+  var completeURL = URL + playerInfo[0][0] + "&PlusMinus=N&Rank=N&Season=" + year + "&SeasonSegment=&SeasonType=Regular+Season&ShotClockRange=&VsConference=&VsDivision="
 
   console.log(completeURL);
-  var season = req.params.year;
-  axios.get(completeURL)
+  return axios.get(completeURL)
     .then((response) => {
       //start oct 31st end april 8th
       //Season=2016-17
       //&SeasonSegment=&SeasonType=Regular+Season&ShotClockRange=&VsConference=&VsDivision=
-      console.log(season);
-      console.log(SEASONDATES[season]);
-      console.log(SEASONDATES[season][0]);
-      var seasonStart = new Date(SEASONDATES[season][0]);
-      var seasonEnd = new Date(SEASONDATES[season][1]);
+
 
       var data = Utils.formatJSONArray(response.data.resultSets[0].headers, response.data.resultSets[0].rowSet);
 
       /*
-      * TODO:
-      * add all of the players to the data array and then sort the weeks
-      */
-      var perWeekData = {};
-      var counter = 0;
+       * TODO:
+       * add all of the players to the data array and then sort the weeks
+       */
 
-      while (seasonStart < seasonEnd) {
-        var week = new Array();
-        var enddate = new Date(seasonStart);
-        var newDate = enddate.setDate(enddate.getDate() + 7);
-        enddate = new Date(newDate);
-        data.map((game) => {
-          var tempDate = new Date(game.GAME_DATE);
-          if (tempDate > seasonStart && tempDate < enddate) {
-            week.push(game)
-          }
-        })
-        perWeekData["week" + counter] = week;
-        seasonStart = new Date(enddate);
-        counter++;
-        console.log(counter)
-      }
 
-      res.json({
-        "test": perWeekData
-      })
+      return data;
     })
+}
+
+function weeklify(data, year){
+  var seasonStart = new Date(SEASONDATES[year][0]);
+  var seasonEnd = new Date(SEASONDATES[year][1]);
+  var perWeekData = {};
+  var counter = 0;
+
+  while (seasonStart < seasonEnd) {
+    var week = new Array();
+    var enddate = new Date(seasonStart);
+    var newDate = enddate.setDate(enddate.getDate() + 7);
+    enddate = new Date(newDate);
+    data.map((game) => {
+      var tempDate = new Date(game.GAME_DATE);
+      if (tempDate > seasonStart && tempDate < enddate) {
+        week.push(game)
+      }
+    })
+    perWeekData["week" + counter] = week;
+    seasonStart = new Date(enddate);
+    counter++;
+    console.log(counter)
+  }
+
+  return perWeekData
+}
+
+
+router.get("/getSeasonStats/:year/:playerName", (req, res) => {
+  getSeasonStatsPlayer(req.params.year, req.params.playerName).then((perWeekData) => {
+    res.json({
+      "test": perWeekData
+    })
+  })
+
+})
+
+function promiseWhile(condition, action) {
+  var resolver = Promise.defer();
+
+  var loop = function() {
+    if (!condition())
+      return resolver.resolve();
+    return Promise.cast(action()).then(loop).catch(resolver.reject);
+  };
+
+  process.nextTick(loop);
+  return resolver.promise;
+};
+
+function teamStats(team){
+
+  var counter = 0;
+  var length = team.length;
+
+  var allData = new Array();
+
+  return promiseWhile(function() {
+    return counter < length;
+  }, function() {
+
+    return getSeasonStatsPlayer("2016-17", team[counter]).then((response) => {
+      allData.push(response);
+      counter++;
+    });
+  }).then((response) => {
+    //console.log(response)
+    var simplifiedData = allData.reduce((sum, value)=>{
+      return sum.concat(value)
+    });
+    return weeklify(simplifiedData, "2016-17");
+  })
+}
+
+router.get("/getExampleTeam", (req, res) => {
+
+  var team = ["Paul-George", "LeBron-James"];
+  teamStats(team).then( (response) => {
+    res.json({test: response})
+  })
+
 })
 
 
